e53d42219a642f44c6ae63e7b7b0a4db937f589c
baziuk-d <damikbazz@gmail.com>
2026-05-17 19:20:06 +0300

    firebase

--- store/decorators/auth.py
+++ store/decorators/auth.py
@@ -1,31 +1,25 @@
 from flask import request, jsonify
 from functools import wraps
-import os
-from google.oauth2 import id_token
-from google.auth.transport import requests as google_requests
+import firebase_admin
+from firebase_admin import auth as firebase_auth, credentials
+
+if not firebase_admin._apps:
+    cred = credentials.ApplicationDefault()
+    firebase_admin.initialize_app(cred)
 
 
 def require_auth(f):
     @wraps(f)
     def decorated(*args, **kwargs):
-
         auth_header = request.headers.get('Authorization', None)
         if not auth_header or not auth_header.startswith('Bearer '):
             return jsonify({'error': 'Authorization header missing or invalid'}), 401
 
         token = auth_header.split(' ')[1]
-        CLIENT_ID = os.getenv('PROJECT_ID')
         try:
-            decoded_token = id_token.verify_firebase_token(
-                token,
-                google_requests.Request(),
-                CLIENT_ID
-            )
-            # Optionally, you can attach decoded_token to request context here
+            firebase_auth.verify_id_token(token)
         except Exception as e:
             return jsonify({'error': 'Invalid token', 'details': str(e)}), 401
 
         return f(*args, **kwargs)
     return decorated
-
-# ...existing code...
